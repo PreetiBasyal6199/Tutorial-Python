fd94a90c52e0968bdc7f6fd1b1cf2b77de242548
Preeti Basyal <preetibasyal412@gmail.com>
2023-03-14 17:53:04 +0545

    merge dictionaries

--- Coding Exercise/Ex1.py
+++ Coding Exercise/Ex1.py
@@ -3,3 +3,11 @@
 markdict={"Tom":67, "Tina": 54, "Akbar": 87, "Kane": 43, "Divya":73}
 sorted_dict = dict(sorted(markdict.items(), key=lambda x: x[1]))
 print(sorted_dict)
+
+# How to merge dictionaries in Python?
+
+d1={'A1': 20, 'B1': 25, 'C1': 40, 'D1': 50}
+d2={"X1":100, "Y1":200, "b1":25, "A1":22,"D1":"Hello"}
+for k,v in d2.items():
+    d1[k]=v
+print(d1)
