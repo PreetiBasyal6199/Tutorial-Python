51e407409e5e1557ba4ae4042322cdea04652cbe
Preeti Basyal <preetibasyal412@gmail.com>
2023-03-24 21:27:17 +0545

    Database created

--- SQL/create_database.py
+++ SQL/create_database.py
@@ -7,6 +7,24 @@ def create_connection(path):
     try:
         connection=sqlite3.connect(path)            # Connection to database
         print("Connection to database success")
+        cu =connection.cursor()
+        #---------------Creating a table----------------------------
+        cu.execute("""                              
+        CREATE TABLE IF NOT EXISTS position(
+        id INTEGER PRIMARY KEY AUTOINCREMENT ,
+        name TEXT NOT NULL
+        ) 
+        """)
+        cu.execute("""
+        CREATE TABLE  IF NOT EXISTS EMPLOYEE(
+        id INTEGER PRIMARY KEY,
+        full_name TEXT NOT NULL,
+        address TEXT NOT NULL,
+        position_id INTEGER NOT NULL,
+        FOREIGN KEY (position_id) REFERENCES position (id) 
+        )
+        """)
+
     except Error as e:
         print(e)
     return connection
